851ca1949af0c03ec9208aa4e4607d9811bca2c1
VaJlek <105868160+VaJlek@users.noreply.github.com>
2022-06-07 20:43:02 +0300

    ошибки линтера

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ getRandom(0, 1);
 const getRandomFloat = (min, max, count) => {
   if (min >= 0 && min < max) {
     return parseFloat((Math.random() * (max - min) + min).toFixed(count));
-  } 
+  }
   throw new Error('Неверный диапозон');
 };
 getRandomFloat (1.1,1.2,5);
