a24c160e3ac98a9710d6339bccc909e33677acb2
Keks <keks@htmlacademy.ru>
2022-06-07 20:20:29 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,10 +1,10 @@
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 getRandom(0, 1);
 
-function getRandomFloat (min, max, count) {
+const getRandomFloat = (min, max, count) => {
   if (min >= 0 && min < max) {
     return parseFloat((Math.random() * (max - min) + min).toFixed(count));
-  } else {
-    throw new Error('Неверный диапозон');}
+  } 
+throw new Error('Неверный диапозон');
 }
 getRandomFloat (1.1,1.2,5);
