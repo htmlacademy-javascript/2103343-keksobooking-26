4bf024ca1d535e17b3546c4d2f84bafd8b38dd70
VaJlek <105868160+VaJlek@users.noreply.github.com>
2022-06-26 21:56:53 +0300

    Исправление замечаний

--- js/create-card.js
+++ js/create-card.js
@@ -1,57 +1,61 @@
 import {createAdvertisementArray} from './data.js';
+import { getRandom } from './util.js';
 
-const randomCard = createAdvertisementArray();
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const fragment = document.createDocumentFragment();
 const cardListElement = document.querySelector('#map-canvas');
+const typesMatch = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+const createCard = (card) => {
 
-randomCard.forEach(({author, offer}) => {
-  const cardElement = cardTemplate.cloneNode(true);
+  card.forEach(({author, offer}) => {
+    const cardElement = cardTemplate.cloneNode(true);
+    const {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos} = offer;
+    cardElement.querySelector('.popup__avatar').src = author.avatar ;
+    cardElement.querySelector('.popup__title').textContent = title;
+    cardElement.querySelector('.popup__text--address').textContent = address;
+    cardElement.querySelector('.popup__text--price').textContent = `${price} ₽/ночь`;
 
-  cardElement.querySelector('.popup__avatar').src = author.avatar ;
-  cardElement.querySelector('.popup__title').textContent = offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = offer.address;
-  cardElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
 
-  const typesMatch = {
-    flat: 'Квартира',
-    bungalow: 'Бунгало',
-    house: 'Дом',
-    palace: 'Дворец',
-    hotel: 'Отель',
-  };
-  cardElement.querySelector('.popup__type').textContent = typesMatch[offer.type];
+    cardElement.querySelector('.popup__type').textContent = typesMatch[type];
 
-  cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+    cardElement.querySelector('.popup__text--capacity').textContent = `${rooms} комнаты для ${guests} гостей`;
+    cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
 
-  const featuresList = cardElement.querySelectorAll('.popup__feature');
-  featuresList.forEach((featureItem) => {
-    if (!offer.features.some((feature) => featureItem.classList.contains(`popup__feature--${feature}`))) {
-      featureItem.remove();
+    const featuresList = cardElement.querySelectorAll('.popup__feature');
+    featuresList.forEach((featureItem) => {
+      if (!features.some((feature) => featureItem.classList.contains(`popup__feature--${feature}`))) {
+        featureItem.remove();
+      }
+    });
+    if (features.length === 0) {
+      featuresList.classList.add('hidden');
     }
+    cardElement.querySelector('.popup__description').textContent = description;
+
+    const photoList = cardElement.querySelector('.popup__photos');
+    const photoElements = photoList.querySelector('.popup__photo');
+    photoElements.remove();
+    photos.forEach((photo) => {
+      const photoElement = photoElements.cloneNode(true);
+      photoElement.src = photo;
+      photoList.append(photoElement);
+    });
+    if (photoList.length === 0) {
+      photoList.classList.add('hidden');
+    }
+
+
+    fragment.append(cardElement);
+
+
   });
-  if (offer.features.length === 0) {
-    featuresList.classList.add('hidden');
-  }
-  cardElement.querySelector('.popup__description').textContent = offer.description;
 
-  const photoList = cardElement.querySelector('.popup__photos');
-  const photoElements = photoList.querySelector('.popup__photo');
-  photoElements.remove();
-  for (let i = 0; i < offer.photos.length; i++) {
-    const photoElement = photoElements.cloneNode(true);
-    photoElement.src = offer.photos[i];
-    photoList.append(photoElement);
-  }
-  if (photoList.length === 0) {
-    photoList.classList.add('hidden');
-  }
-
-  fragment.append(cardElement);
-
-
-});
-
-cardListElement.append(fragment);
-
+  return cardListElement.append(fragment.children[getRandom(0,9)]);
+};
+createCard(createAdvertisementArray());
