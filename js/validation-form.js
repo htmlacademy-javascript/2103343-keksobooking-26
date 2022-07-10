4e69fa9ca13ddf4974ac7a6c75cabb8d8fcb7aa3
Valentin <Maksimov.valentin@gmail.com>
2022-07-10 20:18:20 +0300

    Помощь друга

--- js/validation-form.js
+++ js/validation-form.js
@@ -68,9 +68,57 @@ const validateType = (value) => value >= getMinPrice() && value <= MAX_ROOM_PRIC
 const getAdvPriceErrorMessage = () => `Минимальная цена ${getMinPrice()}, максимальная цена ${MAX_ROOM_PRICE}`;
 
 pristine.addValidator(priceInput, validateType, getAdvPriceErrorMessage);
+//  Слайдер
+const sliderElement = document.querySelector('.ad-form__slider');
 
-const typeChanging = (evt) => {priceInput.placeholder = advTypePrice[evt.target.value];
+noUiSlider.create(sliderElement, {
+  range: {
+    min: getMinPrice(),
+    max: MAX_ROOM_PRICE,
+  },
+  start: 0,
+  step: 100,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      return value.toFixed(0);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+priceInput.addEventListener('input', () => {
+  sliderElement.noUiSlider.set(priceInput.value);
+});
+
+sliderElement.noUiSlider.on('slide', () => {
+  priceInput.value = sliderElement.noUiSlider.get();
   pristine.validate(priceInput);
+});
+
+const getMinPriceForSlider = (minPrice) => {
+  if (minPrice > priceInput.value) {
+    return minPrice;
+  }
+
+  return Number(priceInput.value);
+};
+
+
+const typeChanging = (evt) => {
+  const minRoomPrice = advTypePrice[evt.target.value];
+  priceInput.placeholder = minRoomPrice;
+  priceInput.min = minRoomPrice;
+  pristine.validate(priceInput);
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: 0,
+      max: MAX_ROOM_PRICE,
+    },
+    start: getMinPriceForSlider(minRoomPrice),
+  });
 };
 advTypeField.addEventListener('change', typeChanging);
 
